19c452565141cd6c6ea0d9ce34fb29f5730d5a12
Veenxz-win <42843131+Veenxz@users.noreply.github.com>
2020-11-07 16:28:34 +0800

    update cases

---
+++ 2D_Plane_simpleFoam/run_solver.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+foamCleanTutorials
+blockMesh | tee log.blockMesh
+
+rm -rf 0
+cp -r 0_org 0
+
+decomposePar
+mpirun -np 4 renumberMesh -overwrite -parallel 
+mpirun -np 4 simpleFoam -parallel | tee log.solver
